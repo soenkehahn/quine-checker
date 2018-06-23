225b23795ce8cfa68545a9d01de7bb0794bc62cb
Sönke Hahn <soenkehahn@gmail.com>
2018-06-22 22:09:37 -0400

    detects non-quines correctly

--- src/QuineChecker/Run.hs
+++ src/QuineChecker/Run.hs
@@ -2,6 +2,13 @@
 module QuineChecker.Run where
 
 import System.Exit
+import Development.Shake
+import System.FilePath
 
 run :: String -> IO ExitCode
-run _ = return ExitSuccess
+run directory = do
+  Stdout stdout <- cmd "./quine" (Cwd directory)
+  code <- readFile (directory </> "quine")
+  return $ if code == stdout
+    then ExitSuccess
+    else ExitFailure 1
--- test/QuineChecker/RunSpec.hs
+++ test/QuineChecker/RunSpec.hs
@@ -2,12 +2,14 @@ module QuineChecker.RunSpec where
 
 import QuineChecker.Run
 import System.Directory
+import Development.Shake
 import System.Exit
 import Test.Hspec
 import Test.Mockery.Directory
+import System.IO.Silently
 
 spec :: Spec
-spec = do
+spec = around_ silence $ do
   describe "run" $ do
     describe "when pointed to a directory containing a quine" $ do
       it "exits with exit code 0" $ do
@@ -15,12 +17,34 @@ spec = do
           createDirectory "foo"
           writeFile "foo/quine" $ unlines $
             "#!/usr/bin/env python3" :
-            "" :
+            "import sys" :
             "with open(__file__, 'r') as fh:" :
-            "    print(fh.read())" :
+            "    sys.stdout.write(fh.read())" :
             []
+          unit $ cmd "chmod +x foo/quine"
           run "foo" `shouldReturn` ExitSuccess
+
+      it "allows quines through stderr" $ do
+        pending
+
+      it "prints the quine" $ do
+        pending
 
     describe "when pointed to a directory containing an incorrect quine" $ do
       it "exits with exit code 1" $ do
+        inTempDirectory $ do
+          createDirectory "foo"
+          writeFile "foo/quine" $ unlines $
+            "#!/usr/bin/env python3" :
+            "" :
+            "print('Hello, World!')" :
+            []
+          unit $ cmd "chmod +x foo/quine"
+          run "foo" `shouldReturn` ExitFailure 1
+
+      it "outputs a nice diff" $ do
+        pending
+
+    describe "when ./quine doesn't exist" $ do
+      it "throws a good error message" $ do
         pending
