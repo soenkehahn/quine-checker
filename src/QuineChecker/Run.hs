4951b611afaf88f1b76ccdbec52117035304f2e5
Sönke Hahn <soenkehahn@gmail.com>
2018-06-22 22:33:34 -0400

    support multiple directories

--- src/QuineChecker/Run.hs
+++ src/QuineChecker/Run.hs
@@ -2,11 +2,24 @@
 module QuineChecker.Run where
 
 import System.Exit
-import Development.Shake
+import Development.Shake hiding (doesDirectoryExist)
+import Control.Monad
+import System.Directory
+import Control.Exception
 import System.FilePath
 
-run :: String -> IO ExitCode
-run directory = do
+run :: [String] -> IO ExitCode
+run (directory : rest) = do
+  exitCode <- checkQuine directory
+  case exitCode of
+    ExitSuccess -> run rest
+    ExitFailure _ -> return exitCode
+run [] = return ExitSuccess
+
+checkQuine directory = do
+  directoryExists <- doesDirectoryExist directory
+  when (not directoryExists) $
+    throwIO $ ErrorCall ("directory does not exist: " ++ directory)
   Stdout stdout <- cmd "./quine" (Cwd directory)
   code <- readFile (directory </> "quine")
   return $ if code == stdout
--- test/QuineChecker/RunSpec.hs
+++ test/QuineChecker/RunSpec.hs
@@ -4,29 +4,38 @@ import QuineChecker.Run
 import System.Directory
 import Development.Shake
 import System.Exit
+import System.FilePath
 import Test.Hspec
 import Test.Mockery.Directory
 import System.IO.Silently
 
-writeQuineFile :: [String] -> IO ()
-writeQuineFile lines = do
-  createDirectory "foo"
-  writeFile "foo/quine" $ unlines lines
-  unit $ cmd "chmod +x foo/quine"
+writeQuineFile :: String -> String -> IO ()
+writeQuineFile directory code = do
+  createDirectory directory
+  writeFile (directory </> "quine") code
+  unit $ cmd "chmod +x" (directory </> "quine")
+
+pythonQuine :: String
+pythonQuine = unlines $
+  "#!/usr/bin/env python3" :
+  "import sys" :
+  "with open(__file__, 'r') as fh:" :
+  "    sys.stdout.write(fh.read())" :
+  []
+
+pythonNonQuine :: String
+pythonNonQuine = unlines $
+  "#!/usr/bin/env python3" :
+  "print('Hello, World!')" :
+  []
 
 spec :: Spec
-spec = around_ silence $ do
+spec = around_ (inTempDirectory . silence) $ do
   describe "run" $ do
-    describe "when pointed to a directory containing a quine" $ do
+    describe "when pointed to directories containing a quine" $ do
       it "exits with exit code 0" $ do
-        inTempDirectory $ do
-          writeQuineFile $
-            "#!/usr/bin/env python3" :
-            "import sys" :
-            "with open(__file__, 'r') as fh:" :
-            "    sys.stdout.write(fh.read())" :
-            []
-          run "foo" `shouldReturn` ExitSuccess
+        writeQuineFile "foo" pythonQuine
+        run ["foo"] `shouldReturn` ExitSuccess
 
       it "allows quines through stderr" $ do
         pending
@@ -34,19 +43,35 @@ spec = around_ silence $ do
       it "prints the quine" $ do
         pending
 
+      it "can checks multiple quines" $ do
+        writeQuineFile "foo" pythonQuine
+        writeQuineFile "bar" pythonQuine
+        run ["foo", "bar"] `shouldReturn` ExitSuccess
+
     describe "when pointed to a directory containing an incorrect quine" $ do
       it "exits with exit code 1" $ do
-        inTempDirectory $ do
-          writeQuineFile $
-            "#!/usr/bin/env python3" :
-            "" :
-            "print('Hello, World!')" :
-            []
-          run "foo" `shouldReturn` ExitFailure 1
+        writeQuineFile "foo" pythonNonQuine
+        run ["foo"] `shouldReturn` ExitFailure 1
+
+      it "gives a nice error message" $ do
+        pending
 
       it "outputs a nice diff" $ do
         pending
 
+      it "checks all given directories" $ do
+        writeQuineFile "foo" pythonQuine
+        writeQuineFile "bar" pythonNonQuine
+        run ["foo", "bar"] `shouldReturn` ExitFailure 1
+
     describe "when ./quine doesn't exist" $ do
       it "throws a good error message" $ do
         pending
+
+    describe "when given a directory that doesn't exist" $ do
+      it "gives a nice error message" $ do
+        run ["foo"] `shouldThrow` errorCall "directory does not exist: foo"
+
+      it "checks all given directories" $ do
+        writeQuineFile "foo" pythonQuine
+        run ["foo", "bar"] `shouldThrow` errorCall "directory does not exist: bar"
