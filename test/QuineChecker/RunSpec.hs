378ec4447b4e454061362bcc27e64ea40f596e64
Sönke Hahn <soenkehahn@gmail.com>
2018-06-22 22:12:50 -0400

    DRY up tests

--- test/QuineChecker/RunSpec.hs
+++ test/QuineChecker/RunSpec.hs
@@ -8,20 +8,24 @@ import Test.Hspec
 import Test.Mockery.Directory
 import System.IO.Silently
 
+writeQuineFile :: [String] -> IO ()
+writeQuineFile lines = do
+  createDirectory "foo"
+  writeFile "foo/quine" $ unlines lines
+  unit $ cmd "chmod +x foo/quine"
+
 spec :: Spec
 spec = around_ silence $ do
   describe "run" $ do
     describe "when pointed to a directory containing a quine" $ do
       it "exits with exit code 0" $ do
         inTempDirectory $ do
-          createDirectory "foo"
-          writeFile "foo/quine" $ unlines $
+          writeQuineFile $
             "#!/usr/bin/env python3" :
             "import sys" :
             "with open(__file__, 'r') as fh:" :
             "    sys.stdout.write(fh.read())" :
             []
-          unit $ cmd "chmod +x foo/quine"
           run "foo" `shouldReturn` ExitSuccess
 
       it "allows quines through stderr" $ do
@@ -33,13 +37,11 @@ spec = around_ silence $ do
     describe "when pointed to a directory containing an incorrect quine" $ do
       it "exits with exit code 1" $ do
         inTempDirectory $ do
-          createDirectory "foo"
-          writeFile "foo/quine" $ unlines $
+          writeQuineFile $
             "#!/usr/bin/env python3" :
             "" :
             "print('Hello, World!')" :
             []
-          unit $ cmd "chmod +x foo/quine"
           run "foo" `shouldReturn` ExitFailure 1
 
       it "outputs a nice diff" $ do
